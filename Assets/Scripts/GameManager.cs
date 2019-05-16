55442cbeb3bd7f0a595fea7822198a362651774a
Chalant <strikerklm96@gmail.com>
2019-05-16 13:32:10 -0500

    Render Pixel Perfect Texture

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
 
 	void initCanvas()
 	{
-		resolution = new Vector3Int(256, 256, 32);
+		resolution = new Vector3Int(4, 4, 32);
 
 		outputTexture = new RenderTexture(resolution.x, resolution.y, resolution.z);
 		outputTexture.enableRandomWrite = true;
@@ -32,19 +32,13 @@ public class GameManager : MonoBehaviour
 		outputTexture.filterMode = FilterMode.Point;
 
 		mainCanvas = GameObject.Find("canvas");
-		mainCanvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
-		mainCanvas.GetComponent<Canvas>().worldCamera = Camera.main;
-		mainCanvas.GetComponent<UnityEngine.UI.CanvasScaler>().uiScaleMode = UnityEngine.UI.CanvasScaler.ScaleMode.ScaleWithScreenSize;
-		mainCanvas.GetComponent<UnityEngine.UI.CanvasScaler>().referenceResolution = new Vector2(1920, 1080);
-		mainCanvas.GetComponent<UnityEngine.UI.CanvasScaler>().matchWidthOrHeight = 1.0f;
 
 
 		texture = new Texture2D(outputTexture.width, outputTexture.height);
+		texture.filterMode = FilterMode.Point;
 
 		outputImage = GameObject.Find("canvas/image").GetComponent<UnityEngine.UI.Image>();
 		outputImage.material.mainTexture = texture;
-		outputImage.type = UnityEngine.UI.Image.Type.Simple;
-		outputImage.GetComponent<RectTransform>().sizeDelta = new Vector2(1080, 1080);
 
 
 
@@ -53,7 +47,19 @@ public class GameManager : MonoBehaviour
 		RenderTexture.active = outputTexture;
 		for (int x = 0; x < outputTexture.width; x++)
 			for (int y = 0; y < outputTexture.height; y++)
+			{
+				if (x % 2 == 0)
+				{
+					color.r = 0;
+					color.g = 1;
+				}
+				else
+				{
+					color.r = 1;
+					color.g = 0;
+				}
 				texture.SetPixel(x, y, color);
+			}
 		texture.Apply();
 		RenderTexture.active = null;
 	}
