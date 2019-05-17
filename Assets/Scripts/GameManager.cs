dc18b4974314f24d32c921100b11cd1ab320d73f
Chalant <strikerklm96@gmail.com>
2019-05-16 22:47:53 -0500

    Simple Shader

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,82 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Assets.Scripts;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	Texture2D texture;
+	GasFlowGpu gas;
 
-	Vector3Int resolution;
-
-	//RenderTexture pressureTexture;
-	//ComputeShader pressureShader;
-	//int pressureHandle;
-
-	RenderTexture moveTexture;
-	ComputeShader moveShader;
-	int moveHandle;
-
-	void initCanvas()
+	void InitCanvas()
 	{
-		resolution = new Vector3Int(64, 64, 24);
-
-		moveTexture = new RenderTexture(resolution.x, resolution.y, resolution.z);
-		moveTexture.enableRandomWrite = true;
-		moveTexture.Create();
-		moveTexture.filterMode = FilterMode.Point;
-
-		//inputTexture = new RenderTexture(resolution.x, resolution.y, resolution.z);
-		//inputTexture.enableRandomWrite = true;
-		//inputTexture.Create();
-		//inputTexture.filterMode = FilterMode.Point;
-
-
-		texture = new Texture2D(resolution.y, resolution.y);
-		texture.filterMode = FilterMode.Point;
+		Vector3Int resolution = new Vector3Int(64, 64, 32);
+		gas = new GasFlowGpu(resolution);
 
 		var outputImage = GameObject.Find("canvas/image").GetComponent<UnityEngine.UI.Image>();
-		outputImage.material.mainTexture = moveTexture;
-
-		
-		for (int x = 0; x < resolution.x; x++)
-			for (int y = 0; y < resolution.y; y++)
-			{
-				texture.SetPixel(x, y, Color.black);
-			}
-		texture.SetPixel(52, 51, Color.green);
-
-		texture.SetPixel(51, 50, Color.clear);
-		texture.SetPixel(52, 50, Color.clear);
-		texture.SetPixel(53, 50, Color.clear);
-		texture.Apply();
-
-		RenderTexture.active = moveTexture;
-		Graphics.Blit(texture, moveTexture);
-		RenderTexture.active = null;
-
-		moveShader = Resources.Load<ComputeShader>("move");
-		moveHandle = moveShader.FindKernel(nameof(moveHandle));
-
-		moveShader.SetTexture(moveHandle, nameof(moveTexture), moveTexture);
+		outputImage.material.mainTexture = gas.RenderTexture;
 	}
 
 	void Start()
 	{
-		initCanvas();
-		StartCoroutine(Example());
+		InitCanvas();
+		StartCoroutine(Tick());
 	}
 
-	void Update()
+	IEnumerator Tick()
 	{
-
-	}
-
-	IEnumerator Example()
-	{
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 1000; i++)
 		{
 			yield return new WaitForSeconds(0.70f);
-			moveShader.Dispatch(moveHandle, resolution.x / 8, resolution.y / 8, 1);
+			gas.Tick();
 		}
 	}
 }
---
+++ Assets/Scripts/GasFlowShader.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	struct PressureTile
+	{
+		public float pressure;
+		public int blocked;
+	}
+
+	public class GasFlowGpu
+	{
+		#region Buffer Data
+		ComputeBuffer tileBuffer;
+		PressureTile[] tileArray;
+		#endregion
+
+		#region Shader
+		public RenderTexture RenderTexture;
+		public Vector3Int Resolution;
+		readonly int numThreads = 8;
+
+		ComputeShader shader;
+		int render;
+		#endregion
+
+		public GasFlowGpu(Vector3Int resolution)
+		{
+			Resolution = resolution;
+
+			RenderTexture = new RenderTexture(resolution.x, resolution.y, resolution.z);
+			RenderTexture.enableRandomWrite = true;
+			RenderTexture.Create();
+			RenderTexture.filterMode = FilterMode.Point;
+
+			SetupTiles(resolution);
+			SetupShaders();
+		}
+
+		void SetupTiles(Vector3Int resolution)
+		{
+			// array
+			tileArray = new PressureTile[resolution.x * resolution.y];
+			for (int i = 0; i < tileArray.Length; ++i)
+			{
+				tileArray[i].pressure = 0;
+				tileArray[i].blocked = 0;
+			}
+
+			// buffer
+			tileBuffer = new ComputeBuffer(tileArray.Length, System.Runtime.InteropServices.Marshal.SizeOf(typeof(PressureTile)));
+			tileBuffer.SetData(tileArray);
+		}
+
+		void SetupShaders()
+		{
+			shader = Resources.Load<ComputeShader>("gas");
+			render = shader.FindKernel(nameof(render));
+			shader.SetTexture(render, nameof(RenderTexture), RenderTexture);
+		}
+
+		public void Tick()
+		{
+			shader.Dispatch(render, Resolution.x / numThreads, Resolution.y / numThreads, 1);
+		}
+	}
+}
