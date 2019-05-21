acdda35d39f0322a006cd5c0cab6ca0c081eed61
Chalant <strikerklm96@gmail.com>
2019-05-21 13:51:56 -0500

    Revert "working stable"
    This reverts commit 0badb5591d4ebc928dd356bb3e67114217a15635.

--- Assets/Scripts/GasFlowShader.cs
+++ Assets/Scripts/GasFlowShader.cs
@@ -172,7 +172,7 @@ namespace Assets.Scripts
 			// shader
 			shader = Resources.Load<ComputeShader>("gas");
 
-			// forces
+			// disperse
 			{
 				forces = shader.FindKernel(nameof(forces));
 				tiles.SendTo(forces, shader);
