5bf5581f91977753febbae80dd0ff48a260b69ba
Chalant <strikerklm96@gmail.com>
2019-05-21 12:55:58 -0500

    something

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
 ﻿using Assets.Scripts;
+using GasFlow;
 using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 	GasFlowGpu gas;
+	GasWorld<Tile> gas2;
 
 	void InitCanvas()
 	{
 		// this resolution must match that in gas.compute.index
 		Vector3Int resolution = new Vector3Int(32, 32, 32);
 		gas = new GasFlowGpu(resolution);
+		gas2 = new GasWorld<Tile>(new Vector2Int(resolution.x, resolution.y));
 
 		var outputImage = GameObject.Find("canvas/image").GetComponent<UnityEngine.UI.Image>();
 		outputImage.material.mainTexture = gas.RenderTexture;
