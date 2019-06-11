1731a0a70d1769bbe780bc8d3bddff6d0846d763
Chalant <strikerklm96@gmail.com>
2019-06-10 22:29:15 -0500

    Gas Component

--- Assets/Scripts/GasFlow/GasComponent.cs
+++ Assets/Scripts/GasFlow/GasComponent.cs
@@ -1,10 +1,27 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Game
 {
-	public class GasComponent : MonoBehaviour
+	/// <summary>
+	/// Controls how gas flows.
+	/// </summary>
+	public class GasComponent : GameMonoBehaviour
 	{
+		/// <summary>
+		/// Force this object applies to the surrounding gas.
+		/// </summary>
+		public Vector2 ForceApplication;
+
+		/// <summary>
+		/// True if this should block gas flow.
+		/// </summary>
+		public bool BlocksGasFlow;
+
+		/// <summary>
+		/// Adds or subtracts this much mass per second to this cell.
+		/// </summary>
+		public float DeltaMass;
+		
 		private void Start()
 		{
 
