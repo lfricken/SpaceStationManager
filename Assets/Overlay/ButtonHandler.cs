ff5b4dcc55203321f322ae3ee7995492dfedbe4e
Chalant <strikerklm96@gmail.com>
2019-06-30 20:13:02 -0500

    butt on

---
+++ Assets/Overlay/ButtonHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHandler : Button
+{
+	public override void OnPointerClick(PointerEventData eventData)
+	{
+		base.OnPointerClick(eventData);
+	}
+}
